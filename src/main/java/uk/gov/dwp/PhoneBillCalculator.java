a074e6bb8d7226f86267240fc50d25c2ed6d30bf
stella.martin <stella.martin@engineering.digital.dwp.gov.uk>
2023-09-22 10:31:15 +0100

    Feat: When given one call should return zero

--- src/main/java/uk/gov/dwp/PhoneBillCalculator.java
+++ src/main/java/uk/gov/dwp/PhoneBillCalculator.java
@@ -1,7 +1,7 @@
 package uk.gov.dwp;
 
 public class PhoneBillCalculator {
-    public int calculateBill() {
+    public int calculateBill(String s) {
         return 0;
     }
 }
--- src/test/java/uk/gov/dwp/PhoneBillCalculatorTest.java
+++ src/test/java/uk/gov/dwp/PhoneBillCalculatorTest.java
@@ -3,11 +3,14 @@ package uk.gov.dwp;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBillCalculatorTest {
     @Test
-    @DisplayName("Given one call of one second with no rate identifier will return 3")
-    public void GivenOneCallWillReturnValue(){
+    @DisplayName("Given one call should return zero")
+    public void GivenOneCallWillReturnZero(){
         PhoneBillCalculator phoneBillCalculator = new PhoneBillCalculator();
-        int result = phoneBillCalculator.calculateBill();
+        int result = phoneBillCalculator.calculateBill("00:01:07,400-234-090");
+        assertEquals(0, result, "should return 0 when given one call");
     }
 }
