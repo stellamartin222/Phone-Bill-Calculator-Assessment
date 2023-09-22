6155d9b9f768bce9205d76d2f6a9731587d817e0
stella.martin <stella.martin@engineering.digital.dwp.gov.uk>
2023-09-22 10:49:10 +0100

    Feat: When given two calls, the shortest call being 1 second return 3

--- src/main/java/uk/gov/dwp/PhoneBillCalculator.java
+++ src/main/java/uk/gov/dwp/PhoneBillCalculator.java
@@ -1,7 +1,11 @@
 package uk.gov.dwp;
 
 public class PhoneBillCalculator {
-    public int calculateBill(String s) {
-        return 0;
+    public int calculateBill(String input) {
+        String[] inputArr = input.split("/n");
+            if (inputArr.length == 1){
+                return 0;
+            }
+        return 3;
     }
 }
--- src/test/java/uk/gov/dwp/PhoneBillCalculatorTest.java
+++ src/test/java/uk/gov/dwp/PhoneBillCalculatorTest.java
@@ -8,9 +8,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PhoneBillCalculatorTest {
     @Test
     @DisplayName("Given one call should return zero")
-    public void GivenOneCallWillReturnZero(){
+    public void givenOneReturnZero(){
         PhoneBillCalculator phoneBillCalculator = new PhoneBillCalculator();
         int result = phoneBillCalculator.calculateBill("00:01:07,400-234-090");
         assertEquals(0, result, "should return 0 when given one call");
+    }
+
+    @Test
+    @DisplayName("Given two calls, the shortest call being 3 seconds return the rate")
+    public void givenTwoReturnLowest(){
+        PhoneBillCalculator phoneBillCalculator = new PhoneBillCalculator();
+        int result = phoneBillCalculator.calculateBill("00:00:01,123-456-789/n00:01:07,400-234-090");
+        assertEquals(3, result, "Should return 3 when shortest call is 1 second");
     }
 }
