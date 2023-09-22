0c707c2ab5890d49d141cc29cafad2a7a0deeff2
stella.martin <stella.martin@engineering.digital.dwp.gov.uk>
2023-09-22 12:35:49 +0100

    Refactored code: Given two calls, returns the lowest the rate when call is up to 5 minutes

--- src/main/java/uk/gov/dwp/PhoneBillCalculator.java
+++ src/main/java/uk/gov/dwp/PhoneBillCalculator.java
@@ -1,11 +1,53 @@
 package uk.gov.dwp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PhoneBillCalculator {
     public int calculateBill(String input) {
-        String[] inputArr = input.split("/n");
-            if (inputArr.length == 1){
-                return 0;
+        String[] phoneLogArr = input.split("/n");
+        if (phoneLogArr.length == 1) {
+            return 0;
+        }
+
+        ArrayList<String> callDurationList = new ArrayList<String>();
+        for(String log : phoneLogArr){
+         callDurationList.add(log.substring(0, 8));
+         }
+
+        int shortestCallDuration = 0;
+        for(int i = 0;i < callDurationList.size(); i++){
+            String[] durationArray = callDurationList.get(i).split(":");
+            int time = Integer.valueOf(durationArray[2]);
+            if(shortestCallDuration == 0){
+                shortestCallDuration = time;
             }
-        return 3;
+            if (shortestCallDuration > time){
+                shortestCallDuration = time;
+            }
+        }
+
+        return shortestCallDuration * 3;
     }
 }
+
+//    PhoneCall(List<String> values) {
+//        phoneNumber = values.get(1);
+//        String[] durationArray = values.get(0).split(":");
+//        hours = Integer.valueOf(durationArray[0]);
+//        minutes = Integer.valueOf(durationArray[1]);
+//        seconds = Integer.valueOf(durationArray[2]);
+//        totalSeconds = (hours * 3600) + (minutes * 60) + (seconds);
+//    }
+
+
+
+// get the length of time for each
+// split time?
+// compare times
+
+// calculate lowest cost based on cost per second
+//return cost
+
+    //int compareTimes = time1.compareTo(time2);
